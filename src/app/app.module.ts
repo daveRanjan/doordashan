2e385918fd691e9210974894ef360740d3e03474
monis001 <mmonis@3clogic.com>
2020-10-21 22:40:56 +0530

    buttons module imported

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,9 @@ import { LayoutComponent } from './components/layout/layout.component';
 
 import {GridsterModule} from 'angular-gridster2';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSliderModule } from '@angular/material/slider';
+import { MatSliderModule, } from '@angular/material/slider';
+import { MatButtonModule } from '@angular/material/button' ;
+import { MatIconModule} from '@angular/material/icon' ;
 
 @NgModule({
   declarations: [
@@ -20,6 +22,8 @@ import { MatSliderModule } from '@angular/material/slider';
     GridsterModule,
     BrowserAnimationsModule,
     MatSliderModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
