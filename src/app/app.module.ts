0c89d7a692b1506c9ffb9b88477d82284adbb59c
monis001 <mmonis@3clogic.com>
2020-10-22 22:15:34 +0530

    widget setting component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,16 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule, } from '@angular/material/slider';
 import { MatButtonModule } from '@angular/material/button' ;
 import { MatIconModule} from '@angular/material/icon' ;
+import {MatDialogModule} from '@angular/material/dialog';
+import { WidgetSettingsComponent } from './components/widget-settings/widget-settings.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LayoutComponent
+    LayoutComponent,
+    WidgetSettingsComponent,
   ],
   imports: [
     BrowserModule,
@@ -23,7 +28,8 @@ import { MatIconModule} from '@angular/material/icon' ;
     BrowserAnimationsModule,
     MatSliderModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
