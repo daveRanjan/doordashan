217941cda6748b91fac63a43051fb44a2356cd34
daveRanjan <dtiwari18@gmail.com>
2020-10-26 21:05:16 +0530

    Added interceptor with placeholder code only

---
+++ src/app/interceptor/auth.interceptor.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return undefined;
+  }
+
+}
